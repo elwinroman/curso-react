d67ad9c0a9ea38aff6635df1f0dba2e2013bd675
Elwin Roman <example>
2024-05-29 01:25:26 -0500

    add button and separate code

--- projects/04-prueba-tecnica-catfact/src/App.jsx
+++ projects/04-prueba-tecnica-catfact/src/App.jsx
@@ -1,52 +1,22 @@
-import { useState, useEffect } from 'react'
 import './App.css'
+import { useCatImage } from './hooks/useCatImage'
+import { useCatFact } from './hooks/useCatFact'
 
 export default function App () {
-  const CAT_FACT_URL = 'https://catfact.ninja/fact'
-  const CAT_IMAGE_URL = 'https://cataas.com/cat'
+  const { fact, updateFact } = useCatFact()
+  const { image } = useCatImage({ fact })
 
-  const [fact, setFact] = useState(null)
-  const [urlImage, setUrlImage] = useState(null)
+  const WIDTH_IMAGE = 500
 
-  const WIDTH = 500
-
-  useEffect(() => {
-    fetch(CAT_FACT_URL)
-      .then(response => response.json())
-      .then(data => setFact(data.fact))
-  }, [])
-
-  useEffect(() => {
-    if (!fact) return
-
-    const firstWords = fact.split(' ', 3).join(' ')
-    const fontStyles = {
-      fontColor: 'orange',
-      fontSize: '20',
-      width: WIDTH
-    }
-
-    let mix = ''
-    const entriesFontStyles = Object.entries(fontStyles)
-
-    for (const [i, [key, value]] of entriesFontStyles.entries(fontStyles)) {
-      if (i < entriesFontStyles.length - 1) mix += `${key}=${value}&`
-      else mix += `${key}=${value}`
-    }
-
-    fetch(`${CAT_IMAGE_URL}?json=true`)
-      .then(response => response.json())
-      .then(data => {
-        const url = `${CAT_IMAGE_URL}/${data._id}/says/${firstWords}?${mix}`
-        setUrlImage(url)
-      })
-  }, [fact])
+  const handleClick = () => {
+    updateFact()
+  }
 
   return (
-    <>
+    <div className='container'>
+      <button onClick={ handleClick }>Obtener dato</button>
       {fact && <h2>{fact}</h2> }
-
-      {urlImage && <img src={urlImage} alt='cat image' width={WIDTH}/>}
-    </>
+      {image && <img src={image} alt='cat image' width={WIDTH_IMAGE}/>}
+    </div>
   )
 }

266c8eef68084bd2a01ed47575c945fdd5a6102d
Elwin Roman <example>
2024-06-09 23:11:38 -0500

    chore: Update getRandomFact and getRandomImage with mocking values

--- projects/04-prueba-tecnica-catfact/src/services/facts.js
+++ projects/04-prueba-tecnica-catfact/src/services/facts.js
@@ -3,5 +3,6 @@ const CAT_FACT_URL = 'https://catfact.ninja/fact'
 export async function getRandomFact () {
   const response = await fetch(CAT_FACT_URL)
   const data = await response.json()
-  return data.fact
+  // return data.fact
+  return 'Hola mundo, hola gato'
 }

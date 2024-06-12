e0064cc831a78b13c6317492e887250ae7db6ee8
Elwin Roman <elwinromanv@gmail.com>
2024-06-11 22:49:31 -0500

    chore: Refactor Filter component to use separate options for select and filter inputs

--- projects/01-prueba-tecnica-catfact/src/components/filter/Filter.jsx
+++ projects/01-prueba-tecnica-catfact/src/components/filter/Filter.jsx
@@ -1,38 +1,23 @@
 import './Filter.css'
 import { Select } from './Select'
-import { SELECT_OPTIONS } from '../../constants/filter-options'
+import { SELECT_FILTER_OPTIONS, FILTER_OPTIONS } from '../../constants/filter-options'
 
-export function Filter () {
+export function Filter() {
   return (
     <div className="filter-container">
-      { SELECT_OPTIONS.map(element => (
-          <Select key={element.id} label={ element.label } options={ element.options } />
-      )) }
-
-      <div className="filter-box-container">
-        <label htmlFor="" className="filter-label-base">Blur</label>
-        <input type="text" className="filter-box-base type-input-text" placeholder="30" />
-      </div>
-
-      <div className="filter-box-container">
-        <label htmlFor="" className="filter-label-base">Brightness</label>
-        <input type="text" className="filter-box-base type-input-text" placeholder="30" />
-      </div>
-
-      <div className="filter-box-container">
-        <label htmlFor="" className="filter-label-base">Saturation</label>
-        <input type="text" className="filter-box-base type-input-text" placeholder="30" />
-      </div>
-
-      <div className="filter-box-container">
-        <label htmlFor="" className="filter-label-base">Hue</label>
-        <input type="text" className="filter-box-base type-input-text" placeholder="30" />
-      </div>
-
-      <div className="filter-box-container">
-        <label htmlFor="" className="filter-label-base">Lightness</label>
-        <input type="text" className="filter-box-base type-input-text" placeholder="#000" />
-      </div>
+      {
+        SELECT_FILTER_OPTIONS.map(element => (
+          <Select key={element.id} label={element.label} options={element.options} />
+        ))
+      }
+      {
+        FILTER_OPTIONS.map(element => (
+          <div className="filter-box-container">
+            <label htmlFor={element.id} className={element.class}>{element.text}</label>
+            <input type={element.type} className="filter-box-base type-input-text" placeholder={element.placeholder} />
+          </div>
+        ))
+      }
 
     </div>
   )

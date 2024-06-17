49905c0ea04182e77131daa88acbaca7dd0f7a7b
Elwin Roman <example>
2024-06-16 22:34:58 -0500

    add: Input component to use disabled input color type when select an option

--- projects/01-prueba-tecnica-catfact/src/components/filter/Select.jsx
+++ projects/01-prueba-tecnica-catfact/src/components/filter/Select.jsx
@@ -1,10 +1,18 @@
 import './Filter.css'
 
-export function Select ({ label, options }) {
+export function Select ({ label, options, updateDisabled }) {
+  const CUSTOM_OPTION = 'custom'
+
+  const onChange = (event) => {
+    if (!updateDisabled) return
+
+    const { value } = event.target
+    value === CUSTOM_OPTION ? updateDisabled(false) : updateDisabled(true)
+  }
   return (
     <div className="input-box-container">
       <label className="input-label-base">{label}</label>
-      <select className="input-base type-select">
+      <select className="input-base type-select" onChange={onChange}>
         {options.map(element => (
           <option key={element.value} value={element.value}>{element.name}</option>
         ))}

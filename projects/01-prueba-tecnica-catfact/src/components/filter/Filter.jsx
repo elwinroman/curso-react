64b7c9044f288d9a30f922b58a63ce5090b98c09
Elwin Roman <elwinromanv@gmail.com>
2024-06-23 22:10:32 -0500

    refactor: Improve filter component layout and styling

--- projects/01-prueba-tecnica-catfact/src/components/filter/Filter.jsx
+++ projects/01-prueba-tecnica-catfact/src/components/filter/Filter.jsx
@@ -17,32 +17,51 @@ export function Filter ({ updateFact }) {
   return (
     <div className="filter-container">
       <form action="" onSubmit={handleSubmit}>
+        <div className="form-container">
+          <div className="form-input-box">
+            <section className="filter-wrapp">
+              <h4 className="filter-title">Filter principal</h4>
+              <div className="filter-group">
+                <Select label={tipo.label} options={tipo.options} />
+                <Select label={filter.label} options={filter.options} updateDisabled={updateDisabled}/>
+                <InputColor label="Custom filter" disabled={disabled}/>
+              </div>
+            </section>
 
-        <div className="group">
-          <Select label={tipo.label} options={tipo.options} />
-          <Select label={filter.label} options={filter.options} updateDisabled={updateDisabled}/>
-          <InputColor label="Custom filter" disabled={disabled}/>
+            <section className="filter-wrapp">
+              <h4 className="filter-title">Filter custom (personalizado)</h4>
+              <div className="filter-group">
+              {
+                FILTER_OPTIONS.map(element => (
+                  <Input
+                    key={`prop-${element.id}`}
+                    id={element.id}
+                    text={element.text}
+                    type={element.type}
+                    placeholder={element.placeholder}
+                    inputClass={element.inputClass}
+                    labelClass={element.labelClass}
+                    regex={element.regex}
+                    validationErrorMessage={element.validationErrorMessage}
+                  />
+                ))
+              }
+              </div>
+            </section>
 
-          {
-            FILTER_OPTIONS.map(element => (
-              <Input
-                key={`prop-${element.id}`}
-                id={element.id}
-                text={element.text}
-                type={element.type}
-                placeholder={element.placeholder}
-                inputClass={element.inputClass}
-                labelClass={element.labelClass}
-                regex={element.regex}
-                validationErrorMessage={element.validationErrorMessage}
-              />
-            ))
-          }
+            <section className="filter-wrapp">
+              <h4 className="filter-title">Filter text</h4>
+              <div className="filter-group">
+                <InputColor label="FontColor" disabled={false}/>
+                <InputColor label="FontColor" disabled={false}/>
+              </div>
+            </section>
+          </div>
 
-          <InputColor label="FontColor" disabled={false}/>
+          <div className="form-submit-box">
+            <Button type="submit">Obtener nuevo dato</Button>
+          </div>
         </div>
-
-        <Button type="submit">Obtener nuevo dato</Button>
       </form>
 
     </div>
